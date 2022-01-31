825f87a022a2dbb9e7ebe2b4b606729375cfe45e
moamel hashim <muamal1997@gmail.com>
2022-01-31 10:36:17 -0800

    completed javascript primitives and variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'moamle hashim';
+var isCool = true;
+var totalPets = 1;
+var unknown = null;
+var never =
+console.log('value of full name:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('value of unknown:', unknown);
+console.log('value of never:', never);
+console.log('typeof fullName:', fullName);
+console.log('typeof isCool:', isCool);
+console.log('typeof totalPets:', totalPets);
+console.log('typeof unknown:', unknown);
+console.log('typeof never:', never);
