d14fdf85f333b2002ee2a4be0365ff3b392383c0
moamel hashim <muamal1997@gmail.com>
2022-02-21 11:41:48 -0800

    completed zip

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -2,13 +2,25 @@
 
 /* first we create a function that holds 2 parameters
 function zip(first, second) {
-  step 1 create a variable that holds an empty
+  step 1 we create a variable that holds an empty array
+  const myArray = [];
+  step 2 create a variable that holds another empty array
   let newArray = [];
+  step 3 create a condition that checks if the first array is larger then the second array
+  if it is then we remove the last element from that array
+  if(first.length > second.length) {
+    first.splice(first.length - 1, 1);
+  }
   create a for loop to loop over the first array
   for(let i = 0; i < first.length; i++) {
-    for(let j = 0; j < second.length; j++) {
-      newArray.push(first[i] + second[j]);
-      newArray = []
+      step 4 we push first at i into the new array
+      newArray.push(first[i]);
+      step 5 we push the second at i into the new array
+      newArray.push(second[i]);
+      step 6 we push the newArray into myArray
+      myArray.push(newArray);
+      step 7 we reset the newArray back into an empty array
+      newArray = [];
     }
   }
   return newArray
@@ -19,16 +31,14 @@ function zip(first, second) {
 function zip(first, second) {
   const myArray = [];
   let newArray = [];
+  if (first.length > second.length) {
+    first.splice(first.length - 1, 1);
+  }
   for (let i = 0; i < first.length; i++) {
     newArray.push(first[i]);
     newArray.push(second[i]);
-    if (first.length - 1 !== second.length - 1) {
-      newArray.splice(i, 1);
-    } else {
-      myArray.push(newArray);
-      newArray = [];
-
-    }
+    myArray.push(newArray);
+    newArray = [];
   }
   return myArray;
 }
