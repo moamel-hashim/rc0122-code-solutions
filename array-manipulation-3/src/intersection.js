6930fe63a19136ad8284b8445c1db0be49003d29
moamel-hashim <muamal1997@gmail.com>
2022-02-21 17:15:10 -0800

    completed array-manipulation-3

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,27 @@
 /* exported intersection */
+
+/* first we create a function that holds 2 parameters
+function intersection(first,second) {
+  step 1 create an empty array
+  const newArray = [];
+  step 2 create a for loop to loop over the first array
+  for(let i = 0; i < first.length; i++) {
+    step 3 create conditional that check if indexOf second with the argument of first at i is not equal to - 1 if it not then we push
+    the values into the newArray
+    if(second.indexOf(first[i]) !== -1) {
+      newArray.push(first[i]);
+    }
+  }
+  return newArray
+}
+*/
+
+function intersection(first, second) {
+  const newArray = [];
+  for (let i = 0; i < first.length; i++) {
+    if (second.indexOf(first[i]) !== -1) {
+      newArray.push(first[i]);
+    }
+  }
+  return newArray;
+}
