35122c60ecbbc119e990da7dcf5bfdc748533053
moamel-hashim <muamal1997@gmail.com>
2022-02-23 23:14:56 -0800

    completed javascript-bank

---
+++ javascript-bank/bank.js
@@ -0,0 +1,34 @@
+/* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (Number.isInteger(balance) && balance > 0) {
+    const newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (let i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  let total = 0;
+  for (let i = 0; i < this.accounts.length; i++) {
+    total += this.accounts[i].getBalance();
+  }
+  return total;
+};
