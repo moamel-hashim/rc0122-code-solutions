3cfd9d5a95a71c7fdef0ad07fdf1edaca7c7dbed
moamel-hashim <muamal1997@gmail.com>
2022-03-16 10:57:20 -0700

    completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,10 @@
+console.log('type of export:', typeof exports);
+console.log('the value of exports', exports);
+console.log('type of require:', typeof require);
+console.log('value of require:', require);
+console.log('type of module:', typeof module);
+console.log('value of module:', module);
+console.log('type of __filename:', __filename);
+console.log('value of __filename:', __filename);
+console.log('type of __dirname', __dirname);
+console.log('value of __dirname', __dirname);
