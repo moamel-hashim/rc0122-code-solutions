38c3215097c12562ad39ed46a425afb3452acb09
moamel-hashim <muamal1997@gmail.com>
2022-03-17 12:56:06 -0700

    completed node-fs-writefile

--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -1,6 +1,6 @@
 
 const data = process.argv[2];
 const fs = require('fs');
-fs.writeFile('./note.txt', data, err => {
+fs.writeFile('./note.txt', `${data}\n`, err => {
   if (err) throw err;
 });
--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -1,5 +1,5 @@
 const data = Math.random().toString();
 const fs = require('fs');
-fs.writeFile('./random.txt', data, err => {
+fs.writeFile('./random.txt', `${data}\n`, err => {
   if (err) throw err;
 });
