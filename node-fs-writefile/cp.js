8b94b903a3c10027f195c6c61f8e3bc84417d677
moamel-hashim <muamal1997@gmail.com>
2022-03-18 15:07:02 -0700

    completed node-fs-writefile lvl 3

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -3,7 +3,7 @@ const note = process.argv[2];
 const fs = require('fs');
 fs.readFile(note, 'utf-8', (err, data) => {
   if (err) throw err;
-  fs.writeFile('./reminder.txt', `${data}\n`, err => {
+  fs.writeFile(process.argv[3], `${data}\n`, err => {
     if (err) throw err;
   });
 });
