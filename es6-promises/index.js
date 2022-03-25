5ec513163142d46322ec1f79b629615d990d1cf9
moamel hashim <moamel1997@gmail.com>
2022-03-25 23:29:42 +0000

    completed es6-promises

---
+++ es6-promises/index.js
@@ -0,0 +1,8 @@
+const takeAChance = require('./take-a-chance');
+const chances = takeAChance('moamel');
+chances.then(value => {
+  console.log(value);
+});
+chances.catch(error => {
+  console.error(error);
+});
