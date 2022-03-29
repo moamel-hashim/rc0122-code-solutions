638d338c67fcb54dca5948299d87bed043686305
moamel hashim <moamel1997@gmail.com>
2022-03-29 19:34:22 +0000

    completed es6-classes

--- es6-classes/student.js
+++ es6-classes/student.js
@@ -6,15 +6,13 @@ class Student {
     this.lastName = lastName;
     this.subject = subject;
   }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getIntroduction() {
+    const fullName = this.getFullName();
+    return `Hello, my name is ${fullName} and I am studying ${this.subject}`;
+  }
 }
-
-Student.prototype.getFullName = function () {
-  const { firstName, lastName } = this;
-  return `${firstName} ${lastName}`;
-};
-
-Student.prototype.getIntroduction = function () {
-  const { subject } = this;
-  const fullName = this.getFullName();
-  return `Hello, my name is ${fullName} and I am studying ${subject}.`;
-};
