39739e45fcf6e2f01a0fee0ad1690e0137038ee9
moamel hashim <moamel1997@gmail.com>
2022-03-31 23:54:23 +0000

    completed react-toggle-switch-component

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Toggle from './toggle';
+
+const element = <Toggle/>;
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(element);
---
+++ react-toggle-switch-component/src/toggle.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false,
+      color: ''
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({ toggle: !this.state.toggle });
+  }
+
+  render() {
+    let onOrOff = '';
+    const $body = document.querySelector('body');
+    let { toggle, color } = this.state;
+    if (toggle === false) {
+      onOrOff = 'OFF';
+      $body.className = '';
+    } else {
+      onOrOff = 'ON';
+      color = 'white';
+      $body.className = 'dark';
+      toggle = false;
+    }
+    return (
+      <div className='row'>
+      <label htmlFor="toggle" className='switch'>
+        <input type="checkbox" name="toggle" id="toggle" onClick={this.handleToggle}/>
+        <span className='slider round'></span>
+      </label>
+      <div>
+        <p className={color}>{onOrOff}</p>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Toggle;
