526b747c84ff12939a18d65cb5991553b16623c9
moamel hashim <moamel1997@gmail.com>
2022-04-02 00:15:31 +0000

    completed react-form-controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NewsLetterForm from './form';
+
+const element = <NewsLetterForm/>;
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(element);
