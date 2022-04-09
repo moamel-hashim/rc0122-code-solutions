b222a4cf0963d2fc2892d18dcd9f2317fded59ed
moamel-hashim <muamal1997@gmail.com>
2022-04-08 17:42:33 -0700

    fixed

--- react-context/client/components/navbar.jsx
+++ react-context/client/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import AppContext from '../lib/app-context';
 export default class Navbar extends React.Component {
   render() {
     const { user, handleSignOut } = this.context;
@@ -33,3 +33,4 @@ export default class Navbar extends React.Component {
     );
   }
 }
+Navbar.contextType = AppContext;
